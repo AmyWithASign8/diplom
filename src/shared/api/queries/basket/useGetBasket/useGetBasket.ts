ae7c08dc25c0080b7d3cdf4ae9b14f2d5412726d
DanilFrontend <danilstorage@gmail.com>
2023-06-12 03:26:15 +0500

    fixes

--- src/shared/api/queries/basket/useGetBasket/useGetBasket.ts
+++ src/shared/api/queries/basket/useGetBasket/useGetBasket.ts
@@ -3,12 +3,8 @@ import {$authHost} from "../../../http";
 import {Basket} from "./types";
 
 export const useGetBasket = (id: string | undefined) => useQuery<Basket>('getOneBasket', async () => {
-    try {
         const {data} = await $authHost.get(`api/basket/get_one/${id}`)
         return data
-    }catch (e) {
-        alert('Произошла ошибка получения видов продуктов!')
-    }
 }, {
     keepPreviousData: true
 })
